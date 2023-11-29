223024834c315a4963d23e9cbe009aaecd613581
R-G <21099230+rosegaelle@users.noreply.github.com>
2023-11-29 18:44:14 +0000

    [AdventOfTDD '23] Exercise 1.

--- src/main/java/org/advent/day1/Elf.java
+++ src/main/java/org/advent/day1/Elf.java
@@ -15,10 +15,35 @@ public class Elf implements Comparable<Elf> {
         return totalCalories;
     }
 
-    @Override
-    public int compareTo(Elf arg0) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'compareTo'");
+    /**
+     * Additional calories found by the Elf, to be added to their current total.
+     * @param newCalories New calories quantity.
+     */
+    public void addCalories(int newCalories) {
+        this.totalCalories += newCalories;
     }
 
+    /**
+     * Comparing the number of calories carried by two Elves.
+     * @param arg0 The Elf to be compared against.
+     * @return Whether the current Elf is carrying an amount of calories less than (-1), the same (0), or greater than (+1) that of the Elf it is being compared against.
+     */
+    @Override
+    public int compareTo(Elf arg0) {
+        if (null == arg0) {
+            throw new IllegalArgumentException("Did you forget to specify which Elf should be compared against?");
+        } else {
+            return compareTo(this.getTotalCalories(), arg0.getTotalCalories());
+        }
+    }
+
+    /**
+     * Helper method for comparing two calory quantities.
+     * @param arg0 The LHS of the comparator.
+     * @param arg1 The RHS of the comparator.
+     * @return Whether the LHS less than (-1), the same (0), or greater than (+1) the RHS.
+     */
+    public static int compareTo(int arg0, int arg1) {
+        return (arg0 < arg1) ? -1 : ((arg0 == arg1) ? 0 : 1);
+    }
 }
--- src/test/java/org/advent/day1/TestElfShould.java
+++ src/test/java/org/advent/day1/TestElfShould.java
@@ -1,6 +1,7 @@
 package org.advent.day1;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
@@ -11,26 +12,93 @@ public class TestElfShould {
     @Test
     void have_zero_total_calories_when_created() {
         Elf elf = new Elf();
+
         assertThat(elf.getTotalCalories(), equalTo(0));
     }
 
     @Test
+    void both_have_zero_total_calories_when_created() {
+        Elf elf1 = new Elf();
+        Elf elf2 = new Elf();
+
+        assertThat(elf1.getTotalCalories(), equalTo(0));
+        assertThat(elf2.getTotalCalories(), equalTo(0));
+    }
+
+    @Test
     void have_1000_total_calories_after_adding_1000() {
-       fail("Not yet implemented");
+        int calories = 1000;
+        Elf elf = new Elf();
+
+        elf.addCalories(calories);
+
+        assertThat(elf.getTotalCalories(), equalTo(calories));
+    }
+
+    @Test
+    void both_have_1000_total_calories_after_adding_1000() {
+        int calories = 2000;
+        Elf elf1 = new Elf();
+        Elf elf2 = new Elf();
+
+        elf1.addCalories(calories);
+        elf2.addCalories(calories);
+
+        assertThat(elf1.getTotalCalories(), equalTo(calories));
+        assertThat(elf2.getTotalCalories(), equalTo(calories));
+    }
+
+    @Test
+    void compare_throw_exception_on_null_argument() {
+        int calories = 3000;
+        Elf elf1 = new Elf();
+        elf1.addCalories(calories);
+
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            elf1.compareTo(null);
+        });
+
+        String expectedMessage = "Did you forget to specify which Elf should be compared against?";
+        String actualMessage = exception.getMessage();
+
+        assertTrue(actualMessage.contains(expectedMessage));
     }
 
     @Test
     void compare_return_positive_value_for_elf_with_more_total_calories() {
-        fail("Not yet implemented");
+        int calories = 4000;
+        Elf elf1 = new Elf();
+        elf1.addCalories(calories);
+
+        Elf elf2 = new Elf();
+
+        assertThat(elf1.compareTo(elf2) > 0, equalTo(true));
+        assertThat(Elf.compareTo(elf1.getTotalCalories(), elf2.getTotalCalories()), equalTo(1));
     }
 
     @Test
     void compare_zero_for_equal_elf() {
-        fail("Not yet implemented");
+        int calories = 5000;
+        Elf elf1 = new Elf();
+        elf1.addCalories(calories);
+
+        Elf elf2 = new Elf();
+        elf2.addCalories(calories);
+
+        assertThat(elf1.compareTo(elf2) == 0, equalTo(true));
+        assertThat(Elf.compareTo(elf1.getTotalCalories(), elf2.getTotalCalories()), equalTo(0));
     }
 
     @Test
     void compare_return_negative_value_for_elf_with_less_total_calories() {
-        fail("Not yet implemented");
+        int calories = 6000;
+        Elf elf1 = new Elf();
+        elf1.addCalories(calories);
+
+        Elf elf2 = new Elf();
+        elf2.addCalories(calories * 2);
+
+        assertThat(elf1.compareTo(elf2) < 0, equalTo(true));
+        assertThat(Elf.compareTo(elf1.getTotalCalories(), elf2.getTotalCalories()), equalTo(-1));
     }
 }
